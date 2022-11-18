6643546aa6c130ec85fe889a3e1098eb13f705c0
gdsakelaris <gsakelar@depaul.edu>
2022-11-18 16:51:46 -0600

    fix scrolling

--- rustext/src/main.rs
+++ rustext/src/main.rs
@@ -112,28 +112,46 @@ struct CursorController {
     // keeps track of what line cursor is currently at:
     row_offset: usize,
     column_offset: usize,
+    render_x: usize,
 }
 // INITIALIZES CURSOR POSITION
 impl CursorController {
     fn new(window_size: (usize, usize)) -> CursorController {
         Self {
-            cursor_x: 4,
-            cursor_y: 1,
+            cursor_x: 0,
+            cursor_y: 0,
             screen_columns: window_size.0,
             screen_lines: window_size.1,
             row_offset: 0,
             column_offset: 0,
+            render_x: 0,
         }
     }
 
-    fn scroll(&mut self) {
+    fn get_render_x(&self, row: &Row) -> usize {
+        row.row_content[..self.cursor_x]
+            .chars()
+            .fold(0, |render_x, c| {
+                if c == '\t' {
+                    render_x + (TAB_STOP - 1) - (render_x % TAB_STOP) + 1
+                } else {
+                    render_x + 1
+                }
+            })
+    }
+
+    fn scroll(&mut self, editor_rows: &EditorRows) {
+        self.render_x = 0;
+        if self.cursor_y < editor_rows.number_of_rows() {
+            self.render_x = self.get_render_x(editor_rows.get_editor_row(self.cursor_y))
+        }
         self.row_offset = cmp::min(self.row_offset, self.cursor_y);
         if self.cursor_y >= self.row_offset + self.screen_lines {
             self.row_offset = self.cursor_y - self.screen_lines + 1;
         }
-        self.column_offset = cmp::min(self.column_offset, self.cursor_x);
-        if self.cursor_x >= self.column_offset + self.screen_columns {
-            self.column_offset = self.cursor_x - self.screen_columns + 1;
+        self.column_offset = cmp::min(self.column_offset, self.render_x);
+        if self.render_x >= self.column_offset + self.screen_columns {
+            self.column_offset = self.render_x - self.screen_columns + 1;
         }
     }
 
@@ -270,10 +288,10 @@ impl Output {
 
     // REFRESH SCREEN ()
     fn refresh_screen(&mut self) -> crossterm::Result<()> {
-        self.cursor_controller.scroll();
+        self.cursor_controller.scroll(&self.editor_rows);
         queue!(self.editor_contents, cursor::Hide, cursor::MoveTo(0, 0))?;
         self.draw_lines();
-        let cursor_x = self.cursor_controller.cursor_x - self.cursor_controller.column_offset;
+        let cursor_x = self.cursor_controller.render_x - self.cursor_controller.column_offset;
         let cursor_y = self.cursor_controller.cursor_y - self.cursor_controller.row_offset;
         queue!(
             self.editor_contents,
