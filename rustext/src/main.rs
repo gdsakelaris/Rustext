01acc57f737f359f5a8d08e105956cacf608d0df
gdsakelaris <gsakelar@depaul.edu>
2022-11-21 19:54:44 -0600

    reorganizing

--- rustext/src/main.rs
+++ rustext/src/main.rs
@@ -10,7 +10,6 @@ use std::time::{Duration, Instant};
 use std::{cmp, env, fs};
 
 const TAB_STOP: usize = 8;
-
 struct RawFix;
 impl Drop for RawFix {
     fn drop(&mut self) {
@@ -19,6 +18,50 @@ impl Drop for RawFix {
         Output::clear_screen().expect("Error");
     }
 }
+#[macro_export]
+macro_rules! prompt {
+    ($output:expr,$($args:tt)*) => {{
+        let output:&mut Output = $output;
+        let mut input = String::with_capacity(32);
+        loop {
+            output.status_message.set_message(format!($($args)*, input));
+            output.refresh_screen()?;
+            match Reader.read_key()? {
+                KeyEvent {
+                    code:KeyCode::Enter,
+                    modifiers:KeyModifiers::NONE
+                } => {
+                    if !input.is_empty() {
+                        output.status_message.set_message(String::new());
+                        break;
+                    }
+                }
+                KeyEvent {
+                    code: KeyCode::Esc, ..
+                } => {
+                    output.status_message.set_message(String::new());
+                    input.clear();
+                    break;
+                }
+                KeyEvent {
+                    code: KeyCode::Backspace | KeyCode::Delete,
+                    modifiers: KeyModifiers::NONE,
+                } => { input.pop(); }
+                KeyEvent {
+                    code: code @ (KeyCode::Char(..) | KeyCode::Tab),
+                    modifiers: KeyModifiers::NONE | KeyModifiers::SHIFT,
+                } => input.push(match code {
+                        KeyCode::Tab => '\t',
+                        KeyCode::Char(ch) => ch,
+                        _ => unreachable!(),
+                    }),
+                _=> {}
+            }
+        }
+        if input.is_empty() { None } else { Some (input) }
+    }};
+}
+//
 
 #[derive(Default)]
 struct Row {
@@ -44,56 +87,6 @@ impl Row {
         self.row_content.remove(at);
         EditorRows::render_row(self)
     }
-}
-
-#[macro_export]
-macro_rules! prompt {
-    ($output:expr,$($args:tt)*) => {{
-        let output:&mut Output = $output;
-        let mut input = String::with_capacity(32);
-        loop {
-            output.status_message.set_message(format!($($args)*, input));
-            output.refresh_screen()?;
-            match Reader.read_key()? {
-                KeyEvent {
-                    code:KeyCode::Enter,
-                    modifiers:KeyModifiers::NONE
-                } => {
-                    if !input.is_empty() {
-                        output.status_message.set_message(String::new());
-                        break;
-                    }
-                }
-                KeyEvent {
-                    code: KeyCode::Esc,
-                    ..
-                } => {
-                    output.status_message.set_message(String::new());
-                    input.clear();
-                    break;
-                }
-                /* add the following */
-                KeyEvent {
-                    code: KeyCode::Backspace | KeyCode::Delete,
-                    modifiers: KeyModifiers::NONE,
-                } =>  {
-                    input.pop();
-                }
-                /* end */
-                KeyEvent {
-                    code: code @ (KeyCode::Char(..) | KeyCode::Tab),
-                    modifiers: KeyModifiers::NONE | KeyModifiers::SHIFT,
-                } => input.push(match code {
-                        KeyCode::Tab => '\t',
-                        KeyCode::Char(ch) => ch,
-                        _ => unreachable!(),
-                    }),
-
-                _=> {}
-            }
-        }
-        if input.is_empty() { None } else { Some (input) }
-    }};
 }
 
 struct StatusMessage {
